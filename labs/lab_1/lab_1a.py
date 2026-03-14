b9dc4d7a950f2c4ce4ab4202463c1453efe2dbf7
Nathan Tan <ruiruitan2010@gmail.com>
2026-03-13 20:07:45 -0400

    Added introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Nathan Tan" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello everyone! My name is Nathan, and I am a sophomore from Massachusetts. My favorite subjects are math and physics, but I also enjoy coding. In my free time I enjoy solving cryptograms and playing frisbee.")
 
 if __name__ == "__main__":
     main()
